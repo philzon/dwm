99d71ea1858a1a69faef7eef07a89ba4713530ed
Philzon <7958046+philzon@users.noreply.github.com>
2020-05-09 17:55:57 +0200

    Remove browser binding

--- config.h
+++ config.h
@@ -59,13 +59,11 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *termfallbackcmd[]  = { "xterm", NULL };
-static const char *browsercmd[]  = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_i,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_x,      spawn,          {.v = termfallbackcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
